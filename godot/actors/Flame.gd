e0c88734d4d352ce8f6b8acbe70b62756530cf02
Nathalie Kroeker <nathaliekroeker@gmail.com>
2019-12-04 13:21:40 -0500

    Add speed changing with up and down arrows

--- godot/actors/Flame.gd
+++ godot/actors/Flame.gd
@@ -3,6 +3,10 @@ extends KinematicBody2D
 # Member variables
 export (int) var speed = 200
 export (float) var rotation_speed = 3
+var current_speed = speed
+var delta_speed = 10
+var min_speed = 100
+var max_speed = 700
 var velocity = Vector2()
 var rotation_dir = 0
 var screen_size
@@ -15,18 +19,25 @@ func _ready():
 	# Start with a random direction
 	rotation_dir = deg2rad(rand_range(0, 360))
 	rotate(rotation_dir)
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func check_inputs(delta):
 	rotation_dir = 0
 	if Input.is_action_pressed("ui_right"):
 		rotation_dir += 1
 	if Input.is_action_pressed("ui_left"):
 		rotation_dir -= 1
+	if Input.is_action_pressed("ui_up"):
+		if (current_speed <= max_speed):
+			current_speed += delta_speed
+	if Input.is_action_pressed("ui_down"):
+		if (current_speed >= min_speed):
+			current_speed -= delta_speed
 	rotation += rotation_dir * rotation_speed * delta
 
 	
 func _physics_process(delta):
-	velocity = Vector2(speed, 0).rotated(rotation)
+	check_inputs(delta)
+	velocity = Vector2(current_speed, 0).rotated(rotation)
 	move_and_slide(velocity)
 	
